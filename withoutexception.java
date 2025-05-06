702c51d27c16c632fa3d4b4fe577b50ce12295df
madhusreeja2004 <165631910+madhusreeja2004@users.noreply.github.com>
2025-05-06 12:04:49 +0530

    Program without exception

---
+++ withoutexception.java
@@ -0,0 +1,14 @@
+package exception_handling;
+
+//Demo program for Without Exception:
+
+public class withoutexception {
+	public static void main(String[] args) {
+      int a,b;
+      int result;
+      a=12;
+      b=0;
+      result=a/b;
+      System.out.println(result);
+	}
+}
