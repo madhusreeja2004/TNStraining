fe4c0008736afeca0c7427013a3360e8ce62818c
madhusreeja2004 <165631910+madhusreeja2004@users.noreply.github.com>
2025-05-06 12:06:10 +0530

    Add files via upload

---
+++ exception_handling/Exceptiondemo.java
@@ -0,0 +1,22 @@
+package exception_handling.try_catch;
+
+public class Exceptiondemo {
+	public static void divide(int a,int b) {
+		int result;
+		try {
+			//12/8
+			//16/0
+			result=a/b;
+			//line 11 will execute when no error in 9th line
+			System.out.println(result);	
+		}
+	catch(Exception e) {
+		System.err.println(e.getMessage());
+	}
+		//in final block can be used to close database connections
+		finally {
+			System.out.println("hello welcome");
+		}
+	}
+}
+
---
+++ exception_handling/Exceptionmain.java
@@ -0,0 +1,10 @@
+package exception_handling.try_catch;
+
+//Main programfor ExceptionDemo
+
+public class Exceptionmain {
+	public static void main(String[] args) {
+		Exceptiondemo.divide(12, 3);
+		Exceptiondemo.divide(12, 0);
+	}
+}
